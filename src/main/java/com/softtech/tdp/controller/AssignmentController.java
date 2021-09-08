9e0da814d8b029e93b4319e7da0c04756cd5c6c1
cjmazzarri <cjmazzarri@gmail.com>
2021-09-07 21:19:42 -0500

    🐛Fixed mapping

--- src/main/java/com/softtech/tdp/controller/AssignmentController.java
+++ src/main/java/com/softtech/tdp/controller/AssignmentController.java
@@ -3,17 +3,20 @@ package com.softtech.tdp.controller;
 import com.softtech.tdp.dto.AssignmentResource;
 import com.softtech.tdp.dto.SaveAssignmentResource;
 import com.softtech.tdp.model.Assignment;
+import com.softtech.tdp.model.AssignmentKey;
 import com.softtech.tdp.service.IAssignmentService;
 import com.softtech.tdp.service.IPatientService;
 import com.softtech.tdp.service.ISpecialistService;
+
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
 @RestController
-@RequestMapping("/patients")
+@RequestMapping("/patient/{patientId}/specialist/{specialistId}/assignments")
 public class AssignmentController {
 
     @Autowired
@@ -22,20 +25,32 @@ public class AssignmentController {
     @Autowired
     private IAssignmentService assignmentService;
 
-    @Autowired
-    private IPatientService patientService;
-
-    @Autowired
-    private ISpecialistService specialistService;
-
-    @PostMapping("/{patientId}/assignments")
+    @PostMapping
     public AssignmentResource createAssignment(@PathVariable(name = "patientId") Integer patientId,
-                                               Integer specialistId,
+                                               @PathVariable(name = "specialistId") Integer specialistId,
                                                @Valid @RequestBody SaveAssignmentResource resource){
         return convertToResource(assignmentService.createAssignment(patientId, specialistId, convertToEntity(resource)));
     }
 
-    
+    @GetMapping()
+    public AssignmentResource getAssignmentByIdAndPatientId(@PathVariable(name = "patientId") Integer patientId,
+                                                            @PathVariable(name = "specialistId") Integer specialistId){
+        return convertToResource(assignmentService.getByPatientIdAndSpecialistId(patientId, specialistId));
+    }
+
+    @PutMapping()
+    public AssignmentResource updateAssignment(@PathVariable(name = "patientId") Integer patientId,
+                                               @PathVariable(name = "specialistId") Integer specialistId,
+                                               @Valid @RequestBody SaveAssignmentResource resource){
+        return convertToResource(assignmentService.updateAssignment(patientId, specialistId,
+                convertToEntity(resource)));
+    }
+
+    @DeleteMapping()
+    public ResponseEntity<?> deleteAssignment(@PathVariable(name = "patientId") Integer patientId,
+                                              @PathVariable(name = "specialistId") Integer specialistId){
+        return assignmentService.deleteAssignment(patientId, specialistId);
+    }
 
     private Assignment convertToEntity(SaveAssignmentResource resource){ return mapper.map(resource, Assignment.class); }
 
