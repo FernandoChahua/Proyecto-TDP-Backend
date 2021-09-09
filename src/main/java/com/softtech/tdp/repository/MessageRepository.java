ff2b88e9f904c2fe8955e6284fa2ab562a34649b
cjmazzarri <cjmazzarri@gmail.com>
2021-09-08 19:07:04 -0500

    ✨Added Repository annotation

--- src/main/java/com/softtech/tdp/repository/MessageRepository.java
+++ src/main/java/com/softtech/tdp/repository/MessageRepository.java
@@ -2,6 +2,8 @@ package com.softtech.tdp.repository;
 
 import com.softtech.tdp.model.Message;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MessageRepository extends JpaRepository<Message, Integer> {
 }
